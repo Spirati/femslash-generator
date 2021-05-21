ce20526bef1c7db27ad8ead81290a207d02d2dec
Lynne Cure <lynne@lynnux.org>
2021-05-21 01:14:19 -0400

    Fix age gaps, clean up creepiness checker

--- app.py
+++ app.py
@@ -74,6 +74,25 @@ def are_you_a_real_human_woman_with_a_real_birthdate(page: WikiLink) -> Union[bo
     else:
         return False
 
+
+def valid_pairing(a: Name, b: Name, women: Dict[Name, Age], reference_year: int) -> bool:
+    a_obj, b_obj = [{
+        "name": name,
+        "last_name": name.split(" ")[-1],
+        "age": reference_year - women[name]
+    } for name in (a,b)]
+
+    related = a_obj["last_name"] == b_obj["last_name"]
+    fey_blood = {a_obj["last_name"], b_obj["last_name"]} in ({"Fey", "Hawthorne"}, {"Iris", "Fey"})
+    lower_a = a_obj["age"] < (7 + b_obj["age"]/2) # a is under the lower bound
+    lower_b = b_obj["age"] < (7 + a_obj["age"]/2) # b is under the lower bound
+    upper_a = a_obj["age"] > (2 * (b_obj["age"] - 7)) # a is over the upper bound
+    upper_b = b_obj["age"] > (2 * (a_obj["age"] - 7)) # b is over the upper bound
+    minor_gap_a = a_obj["age"] < 18 and b_obj["age"] > 18
+    minor_gap_b = b_obj["age"] < 18 and a_obj["age"] > 18
+
+    return not any((related, fey_blood, lower_a, lower_b, upper_a, upper_b, minor_gap_a, minor_gap_b)) 
+
 def random_pairing(women: Dict[Name, Age], reference_year: int, polycule: int) -> Tuple[Tuple[Name], Tuple[Age]]:
     candidates = list(women.keys())
 
@@ -81,6 +100,7 @@ def random_pairing(women: Dict[Name, Age], reference_year: int, polycule: int) -
 
     candidate = random.choice(candidates)
 
+    """
     eligible_bachelorettes = list(filter(
         lambda bachelorette:
             bachelorette != candidate and
@@ -94,18 +114,29 @@ def random_pairing(women: Dict[Name, Age], reference_year: int, polycule: int) -
             not ((reference_year - women[bachelorette] < 18 and reference_year - women[candidate] > 18) or (reference_year - women[bachelorette] > 18 and reference_year - women[candidate] < 18)),
         candidates
     ))
+    """
+    eligible_bachelorettes = list(filter(
+        lambda bachelorette: valid_pairing(candidate, bachelorette, women, reference_year),
+        candidates
+    ))
+
+    if candidate in eligible_bachelorettes: eligible_bachelorettes.remove(candidate)
 
     picks = set([candidate])
 
     if len(eligible_bachelorettes) == 0:
-        return candidate, "No one...", reference_year - women[candidate], "N/A"
+        return (candidate, "No one..."), (reference_year - women[candidate], "N/A")
     while len(picks) < polycule:
         try:
             choice = random.choice(eligible_bachelorettes)
         except:
             break
         picks.add(choice)
-        eligible_bachelorettes.remove(choice)
+        if choice in eligible_bachelorettes: eligible_bachelorettes.remove(choice)
+        eligible_bachelorettes = list(filter(
+            lambda bachelorette: all([valid_pairing(c, bachelorette, women, reference_year) for c in tuple(picks)]),
+            eligible_bachelorettes
+        ))
     
     picks = tuple(picks)
     ages = tuple([reference_year - women[pick] for pick in picks])
